1ac2b3218c4683d0340660ae3f1ba53359110b38
Manav-Kapila <52825227+Manav-Kapila@users.noreply.github.com>
2020-05-30 19:23:41 +0530

    Add files via upload

---
+++ MyWebsite/javaScript/index.js
@@ -0,0 +1,17 @@
+ 
+alert('Hover over each element to see animations');
+
+function dropdownMenu() {
+    'use strict';
+    
+    var x = document.getElementById("dropdownClick");
+    if ( x.className === "topnav")
+    {
+        x.className += " responsive"
+    /* ***** change class from topnav to topnav.responsive  *****  */  
+    }
+    else 
+    {
+        x.className = "topnav";
+    }
+}
